256a60506f74c0c0eab29c30d1a69b81b5286cf1
WP8024 <eco2332@gmail.com>
2022-05-26 12:40:39 +0900

    최종업데이트

--- Assets/GameProject/01 Scenes/TutorialStage.cs
+++ Assets/GameProject/01 Scenes/TutorialStage.cs
@@ -13,19 +13,27 @@ public class TutorialStage : MonoBehaviour
     [SerializeField]
     private DialogSystem dialogSystem04;
 
+
+    public GameObject Tutorial1;
+    public GameObject Tutorial2;
+    public GameObject Tutorial3;
+    public GameObject Tutorial4;
     public GameObject Group1;
-    public GameObject Group2;
-    public GameObject Group3;
+
 
     bool test1=false;
 
     private IEnumerator Start()
     {
+        
         Group1.SetActive(false);
-        Group2.SetActive(false);
-        Group3.SetActive(false);
 
+        Tutorial1.SetActive(false);
+        Tutorial2.SetActive(false);
+        Tutorial3.SetActive(false);
+        Tutorial4.SetActive(false);
         yield return new WaitUntil(() => dialogSystem01.UpdateDialog());
+        Tutorial1.SetActive(true);
 
 
         while (test1)
@@ -37,24 +45,42 @@ public class TutorialStage : MonoBehaviour
         }
 
         yield return StartCoroutine(Wait());
-
+        Tutorial1.SetActive(false);
         yield return new WaitUntil(() => dialogSystem02.UpdateDialog());
-        
-        
-        
+        Tutorial2.SetActive(true);
+
+        yield return StartCoroutine(Wait());
+
         Group1.SetActive(true);
-
-
+        Tutorial2.SetActive(false);
 
 
 
         yield return new WaitUntil(() => dialogSystem03.UpdateDialog());
+        Tutorial3.SetActive(true);
 
 
+        yield return StartCoroutine(Wait());
+        Tutorial3.SetActive(false);
+
+
+        yield return new WaitUntil(() => dialogSystem04.UpdateDialog());
+        Tutorial4.SetActive(true);
+
+
+        yield return StartCoroutine(Wait());
+        Tutorial4.SetActive(false);
+
+        Destroy(Group1);
     }
 
     IEnumerator Wait()
     {
         yield return new WaitForSecondsRealtime(4f);
+    }
+
+    public void OnNext()
+    {
+        LoadingSceneController.LoadScene("Stage1 Scene");
     }
 }
